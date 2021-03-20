ce2e9b21746a0e9d66faa56acb0c0dc274a2ff61
datnikon <vandat03@gmail.com>
2021-03-20 17:14:43 +0700

    Modify API

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -105,7 +105,7 @@ export default {
   created() {
     const slug = this.$route.params.slug;
     get(`products/${slug}`).then((data) => {
-      this.product = data[0];
+      this.product = data.data[0];
     });
   },
   methods: {
